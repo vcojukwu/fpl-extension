a7b8cb17387168d6768811cf2b2735518c283262
Victor Ojukwu <victor.ojukwu94@gmail.com>
2017-12-04 20:50:27 -0500

    change to page action and change folder structure

---
+++ scripts/background.js
@@ -0,0 +1,14 @@
+chrome.pageAction.onClicked.addListener(function(tab) {
+  alert("Page Action Clicked");
+});
+
+function checkForValidUrl(tabId, changeInfo, tab) {
+  if (tab.url.indexOf("fantasy.premierleague.com/a/leagues/standings") > -1) {
+    chrome.pageAction.show(tabId);
+    tablink = tab.url;
+  } else {
+    chrome.pageAction.hide(tabId);
+  }
+};
+
+chrome.tabs.onUpdated.addListener(checkForValidUrl);
