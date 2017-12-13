e6e4114c906d7e343ea57ca794d3c3e667ec90a2
Victor Ojukwu <victor.ojukwu94@gmail.com>
2017-12-13 01:16:44 -0500

    getting data complete

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,7 +1,9 @@
 var gameweeks;
 var gwNumber;
+var gwFixtures;
 var playerInfo;
 var clubs;
+teamsState = [];
 
 function getInfo(leagueUrl) {
   $.get("https://fantasy.premierleague.com/drf/bootstrap-static", function(data) {
@@ -9,7 +11,12 @@ function getInfo(leagueUrl) {
     gameweeks = data.events;
     gwNumber = data['current-event'];
     clubs = data.teams;
-    getLeagueInfo(leagueUrl);
+
+    var live_event_url = 'https://fantasy.premierleague.com/drf/event/' + gwNumber + '/live';
+    $.get(live_event_url, function(data) {
+      gwFixtures = data.fixtures;
+      getLeagueInfo(leagueUrl);
+    });
   });
 }
 
@@ -26,24 +33,70 @@ function getLeagueInfo(leagueUrl) {
 
   $.get(url, function(data) {
     var teams = data.standings.results;
-
-    for(var i in teams) {
-      teamUrl = "https://fantasy.premierleague.com/drf/entry/" + teams[i].entry + "/event/" + gwNumber + "/picks";
+    $.each(teams, function (i, item) {
       var teamPicks;
+      var played = [];
+      var playing = [];
+      var notPlayed = [];
+      var state = {};
+
+      state['name'] = teams[i]['entry_name'];
+      teamsState[i] = state;
+
+      var teamUrl = "https://fantasy.premierleague.com/drf/entry/" + teams[i].entry + "/event/" + gwNumber + "/picks";
       $.get(teamUrl, function(data) {
         teamPicks = data.picks;
+
         for(var j in teamPicks) {
           var currentSelection = teamPicks[j];
           var selectionIdIndex = currentSelection.element - 1;
           //if not benched
           var playerObj = playerInfo[selectionIdIndex];
           var clubObj = clubs[playerObj.team -1];
-          console.log(playerObj['web_name'], clubObj.name);
+
+          var current_fixture_id = clubObj['current_event_fixture'][0].id;
+          var club_current_fixture;
+
+          for (var k = 0; k < gwFixtures.length; k++) {
+            if (gwFixtures[k].id === current_fixture_id) {
+              club_current_fixture = gwFixtures[k];
+              break;
+            }
+          }
+
+          var obj = {
+            name: playerObj['web_name'],
+            isCaptain: currentSelection['is_captain']
+          };
+
+          if (!club_current_fixture.started) {
+            notPlayed.push(obj);
+          } else if (club_current_fixture.started) {
+              if (club_current_fixture.finished) {
+                played.push(obj);
+              } else {
+                playing.push(obj);
+              }
+          }
+        }
+
+        state['played'] = played;
+        state['playing'] = playing;
+        state['notPlayed'] = notPlayed;
+        teamsState[i] = state;
+
+        if (i === teams.length -1) {
+          var event = new Event('ready');
+          window.dispatchEvent(event);
         }
       });
-    }
+    });
   });
 }
+
+window.addEventListener("ready", function() {
+  console.log(teamsState);
+});
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
