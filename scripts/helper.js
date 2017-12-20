02e6f6d976fd2d56ef0c42be4434d036262f4d72
Victor Ojukwu <victor.ojukwu94@gmail.com>
2017-12-19 23:41:12 -0500

    refactor and rewrite using es6 syntax

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,228 +1,198 @@
-var gameweeks;
-var gwNumber;
-var gwFixtures;
-var playerInfo;
-var clubs;
-teamsState = [];
+let clubs;
+let gameWeekNumber;
+let gameWeekFixtures;
+let leagueUrl;
+let playersInformation;
+let teamsState = [];
+let count = 0;
 
-function getInfo(leagueUrl) {
-  $.get("https://fantasy.premierleague.com/drf/bootstrap-static", function(data) {
-    playerInfo = data.elements;
-    gameweeks = data.events;
-    gwNumber = data['current-event'];
-    clubs = data.teams;
+const NOTPLAYED = 1;
+const PLAYING = 2;
+const PLAYED = 3;
 
-    var live_event_url = 'https://fantasy.premierleague.com/drf/event/' + gwNumber + '/live';
-    $.get(live_event_url, function(data) {
-      gwFixtures = data.fixtures;
-      getLeagueInfo(leagueUrl);
-    });
+function getJSON(url) {
+  return fetch(url).then(function(response) {
+    return response.json();
   });
 }
 
-function getLeagueInfo(leagueUrl) {
-  var url;
-  var league_code = leagueUrl.split("/").slice(-2)[0];
+function calculatePoints(playerId) {
+  let playerUrl = `https://fantasy.premierleague.com/drf/element-summary/${playerId}`;
 
-  if(leagueUrl.indexOf("classic") > -1) {
-    url = "https://fantasy.premierleague.com/drf/leagues-classic-standings/" + league_code;
+  return getJSON(playerUrl).then((data) => {
+    let pointCategories = data.explain[0].explain;
+    let points = Object.values(pointCategories).reduce((a, b) => {
+      return a + b.points;
+    }, 0);
+    return points;
+  });
+}
+
+function getClubCurrentFixture(club) {
+  let currentFixtureId = club['current_event_fixture'][0].id;
+
+  for (let i = 0; i < gameWeekFixtures.length; i++) {
+    if (gameWeekFixtures[i].id === currentFixtureId) {
+       return gameWeekFixtures[i];
+    }
   }
-  else if(leagueUrl.indexOf("h2h") > -1) {
-    url = "https://fantasy.premierleague.com/drf/leagues-h2h-standings/" + league_code;
+}
+
+function playingStatus(fixture) {
+  if (fixture.started) {
+    if (fixture.finished) {
+      return PLAYED;
+    } else {
+      return PLAYING;
+    }
+  } else {
+    return NOTPLAYED;
   }
+}
 
-  $.get(url, function(data) {
-    var teams = data.standings.results;
-    $.each(teams, function (i, item) {
-      var teamPicks;
-      var played = [];
-      var playing = [];
-      var notPlayed = [];
-      var state = {};
+function getInfo() {
+  getJSON('https://fantasy.premierleague.com/drf/bootstrap-static').then((data) => {
+    clubs = data.teams;
+    gameWeekNumber = data['current-event'];
+    playersInformation = data.elements;
 
-      state['name'] = teams[i]['entry_name'];
-      state['id'] = teams[i]['entry'];
-      teamsState[i] = state;
+    let liveEventUrl = `https://fantasy.premierleague.com/drf/event/${gameWeekNumber}/live`;
 
-      var teamUrl = "https://fantasy.premierleague.com/drf/entry/" + teams[i].entry + "/event/" + gwNumber + "/picks";
-      $.get(teamUrl, function(data) {
-        teamPicks = data.picks;
-        $.each(teamPicks, function (j, item) {
-          var currentSelection = teamPicks[j];
-          var selectionIdIndex = currentSelection.element - 1;
-          //if not benched
-          var playerObj = playerInfo[selectionIdIndex];
+    getJSON(liveEventUrl).then((data) => {
+      gameWeekFixtures = data.fixtures;
 
-          var playerUrl = "https://fantasy.premierleague.com/drf/element-summary/" + currentSelection.element;
-          var points = 0;
+      getJSON(leagueUrl).then((data) => {
+        let teams = data.standings.results;
 
-          $.ajaxSetup({async:false});
-          $.get(playerUrl, function(data) {
-            var point_categories = data.explain[0].explain;
-            for (var p in point_categories) {
-              points += point_categories[p].points;
-            }
+        teams.map(function(team) {
+          let points;
+          let teamUrl = `https://fantasy.premierleague.com/drf/entry/${team.entry}/event/${gameWeekNumber}/picks`;
+          let teamIndex = team.rank - 1;
+
+          getJSON(teamUrl).then((data) => {
+            data.picks.map((pick) => {
+              let player = playersInformation[pick.element - 1];
+              let club = clubs[player.team - 1];
+              let isCaptain = pick['is_captain'];
+
+              calculatePoints(pick.element).then((result) => {
+                points = result;
+                count++;
+
+                let clubFixture = getClubCurrentFixture(club);
+                let status = playingStatus(clubFixture);
+
+                let obj = {
+                  name: player['web_name'],
+                  isCaptain,
+                  points,
+                  status,
+                };
+
+                if (teamsState[teamIndex]) {
+                  teamsState[teamIndex].push(obj);
+                } else {
+                  teamsState[teamIndex] = [obj];
+                }
+
+                if (count === teams.length * 15) {
+                  let event = new Event('ready');
+                  window.dispatchEvent(event);
+                }
+              });
+            });
           });
-          $.ajaxSetup({async:true});
-
-          var clubObj = clubs[playerObj.team -1];
-
-          var current_fixture_id = clubObj['current_event_fixture'][0].id;
-          var club_current_fixture;
-
-          for (var k = 0; k < gwFixtures.length; k++) {
-            if (gwFixtures[k].id === current_fixture_id) {
-              club_current_fixture = gwFixtures[k];
-              break;
-            }
-          }
-
-          var obj = {
-            name: playerObj['web_name'],
-            isCaptain: currentSelection['is_captain'],
-            points: points
-          };
-
-          if (!club_current_fixture.started) {
-            notPlayed.push(obj);
-          } else if (club_current_fixture.started) {
-              if (club_current_fixture.finished) {
-                played.push(obj);
-              } else {
-                playing.push(obj);
-              }
-          }
         });
-
-        state['played'] = played;
-        state['playing'] = playing;
-        state['notPlayed'] = notPlayed;
-        teamsState[i] = state;
-
-        if (i === teams.length -1) {
-          var event = new Event('ready');
-          window.dispatchEvent(event);
-        }
       });
     });
   });
 }
 
 function buildTooltip(team) {
-  var played = team.played;
-  var playing = team.playing;
-  var notPlayed = team.notPlayed;
+  let playedBody = '';
+  let playingBody = '';
+  let notPlayedBody = '';
 
-  var played_body = '';
-  var playing_body = '';
-  var not_played_body = '';
+  let wrapperStart = `<div class="tooltip-text">`;
+  let wrapperEnd =   `</div>`;
 
-  var wrapper_start = `<div class="tooltip-text">`;
-  var wrapper_end =   `</div>`;
-
-  if (played.length > 0) {
-    var played_body_start = `<h3 class="tooltip-title">Played</h1>
+  let playedBodyStart = `<h3 class="tooltip-title">Played</h3>
                             <table class="tooltip-table">
                               <tbody>`;
 
-    var played_body_end =  `</tbody>
-                          </table>`;
-
-    played_body = played_body_start;
-
-    for (var i in played) {
-      var name = played[i].name;
-      var points = played[i].points;
-      var isCaptain = played[i].isCaptain;
-      if(played_body) {
-        if (isCaptain) {
-          played_body += `<tr><td><b>${name} (${points})</b></td</tr>`;
-        } else {
-          played_body += `<tr><td>${name} (${points})</td</tr>`;
-        }
-      }
-    }
-
-    played_body += played_body_end;
-  }
-
-  if (playing.length > 0) {
-    var playing_body_start = `<h3 class="tooltip-title">Playing</h1>
+  let playingBodyStart = `<h3 class="tooltip-title">Playing</h3>
                             <table class="tooltip-table">
                               <tbody>`;
 
-    var playing_body_end =  `</tbody>
-                          </table>`;
-
-    playing_body = playing_body_start;
-
-    for (var i in playing) {
-      var name = playing[i].name;
-      var points = playing[i].points;
-      var isCaptain = playing[i].isCaptain;
-      if(playing_body) {
-        if (isCaptain) {
-          playing_body += `<tr><td><b>${name} (${points})</b></td</tr>`;
-        } else {
-          playing_body += `<tr><td>${name} (${points})</td</tr>`;
-        }
-      }
-    }
-
-    playing_body += playing_body_end;
-  }
-
-  if (notPlayed.length > 0) {
-    var not_played_body_start = `<h3 class="tooltip-title">Not Played</h1>
+  let notPlayedBodyStart = `<h3 class="tooltip-title">Not Played</h3>
                             <table class="tooltip-table">
                               <tbody>`;
 
-    var not_played_body_end =  `</tbody>
-                          </table>`;
+  let bodyEnd =  `</tbody>
+                    </table>`;
 
-    not_played_body = not_played_body_start;
+  for (let player of team) {
+    let playerRow = `<tr><td>${player.name} (${player.points})</td</tr>`;
+    let playerRowBold = `<tr><td><b>${player.name} (${player.points})</b></td</tr>`;
 
-    for (var i in notPlayed) {
-      var name = notPlayed[i].name;
-      var isCaptain = notPlayed[i].isCaptain;
-      if(not_played_body) {
-        if (isCaptain) {
-          not_played_body += `<tr><td><b>${name}</b></td</tr>`;
-        } else {
-          not_played_body += `<tr><td>${name}</td</tr>`;
-        }
-      }
+    switch (player.status) {
+      case PLAYED:
+        playedBody += player.isCaptain ? playerRowBold : playerRow;
+        break;
+      case PLAYING:
+        playingBody += player.isCaptain ? playerRowBold : playerRow;
+        break;
+      case NOTPLAYED:
+        notPlayedBody += player.isCaptain ? playerRowBold : playerRow;
+        break;
     }
-
-    not_played_body += not_played_body_end;
   }
 
-  return wrapper_start + played_body + playing_body + not_played_body + wrapper_end;
+
+  if (playedBody) {
+    playedBody = playedBodyStart + playedBody + bodyEnd;
+  }
+
+  if (playingBody) {
+    playingBody = playingBodyStart + playingBody + bodyEnd;
+  }
+
+  if (notPlayedBody) {
+    notPlayedBody = notPlayedBodyStart + notPlayedBody + bodyEnd;
+  }
+
+  return wrapperStart + playedBody + playingBody + notPlayedBody + wrapperEnd;
 }
 
 window.addEventListener("ready", function() {
   console.log(teamsState);
-  var links = $('a[href^="/a/team/"]').splice(2);
+  let links = $('a[href^="/a/team/"]').splice(2);
 
-  for (var i in links) {
-    var link = links[i];
-    var team = teamsState[i];
+  for (let i = 0; i < links.length; i++) {
+    let link = links[i];
+    let team = teamsState[i];
 
-    var team_id = link.href.split('/').slice(-1)[0];
-    var str = `a[href="/a/team/${team_id}"]`;
-    var linkElem = $(str);
+    let content = buildTooltip(team);
 
-    var content = buildTooltip(team);
-    linkElem.addClass('tooltip2')
-      .append(content);
+    let teamId = link.href.split('/').slice(-1)[0];
+    $(`a[href="/a/team/${teamId}"]`).addClass('tooltip2').append(content);
   }
-
 });
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    //getInfo(request.url);
     setTimeout(function() {
-      getInfo(request.url);
+      let leagueCode = request.url.split("/").slice(-2)[0];
+
+      if(request.url.includes('classic')) {
+        leagueUrl = `https://fantasy.premierleague.com/drf/leagues-classic-standings/${leagueCode}`;
+      } else if (request.url.includes('h2h')) {
+        leagueUrl = `https://fantasy.premierleague.com/drf/leagues-h2h-standings/${leagueCode}`;
+      } else {
+        console.log('Unknown League Type');
+      }
+      getInfo();
     }, 5000);
 });
+
+
